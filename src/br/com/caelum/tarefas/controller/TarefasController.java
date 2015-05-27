d141b84dacb92b9bde7fc760f6d29a8071d29f7e
Nelma <nelmasb@gmail.com>
2015-05-27 19:53:15 -0300

    Integrando JPA com Spring

--- src/br/com/caelum/tarefas/controller/TarefasController.java
+++ src/br/com/caelum/tarefas/controller/TarefasController.java
@@ -4,23 +4,21 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import br.com.caelum.tarefas.dao.JdbcTarefaDao;
+import br.com.caelum.tarefas.dao.TarefaDao;
 import br.com.caelum.tarefas.modelo.Tarefa;
 
+@Transactional
 @Controller
 public class TarefasController {
 	
-	private final JdbcTarefaDao dao;
-	
 	@Autowired
-	public TarefasController(JdbcTarefaDao dao) {
-		this.dao = dao;
-	}
+	TarefaDao dao;
 
 	@RequestMapping("novaTarefa")
 	public String form() {
--- src/br/com/caelum/tarefas/jpa/BuscaTarefa.java
+++ src/br/com/caelum/tarefas/jpa/BuscaTarefa.java
@@ -1,20 +1,31 @@
 package br.com.caelum.tarefas.jpa;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 import br.com.caelum.tarefas.modelo.Tarefa;
 
-public class CarregaTarefa {
+public class BuscaTarefa {
 
 	public static void main(String[] args) {
 
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory("tarefas");
 		EntityManager manager = factory.createEntityManager();
 		
-		Tarefa encontrada = manager.find(Tarefa.class, 2L);
-		System.out.println(encontrada.getDescricao());
+		Query query = manager
+				.createQuery("select t from Tarefa as t " + 
+					"where t.finalizado = :paramFinalizado");
+		query.setParameter("paramFinalizado", true);
+		
+		List<Tarefa> lista = query.getResultList();
+		
+		for (Tarefa t : lista) {
+			System.out.println(t.getDescricao());
+		}
 		
 		manager.close();
 	}
